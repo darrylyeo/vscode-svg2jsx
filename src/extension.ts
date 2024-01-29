91198a71558ffbea72d198a5c8ba44d189b756b4
Darryl Yeo <dev@darryl-yeo.com>
2024-01-28 17:42:19 -0800

    Extension: register as formatter for .jsx / .tsx

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, languages, TextDocument, TextEdit } from 'vscode'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "svg2jsx" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('svg2jsx.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from svg2jsx!');
-	});
-
-	context.subscriptions.push(disposable);
+export const activate = (context: ExtensionContext) => {
+	languages.registerDocumentFormattingEditProvider(
+		[
+			'javascriptreact',
+			'typescriptreact',
+		],
+		{
+			provideDocumentFormattingEdits: (document: TextDocument): TextEdit[] => {
+				const firstLine = document.lineAt(0)
+				return [TextEdit.insert(firstLine.range.start, '42\n')]
+			}
+		}
+	)
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
