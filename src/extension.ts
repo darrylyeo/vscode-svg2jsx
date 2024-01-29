59b9280f9ca366333b7108322a47822c5bded898
Darryl Yeo <dev@darryl-yeo.com>
2024-01-28 18:14:00 -0800

    Extension: detect and reformat SVG as JSX

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,61 @@
-import { ExtensionContext, languages, TextDocument, TextEdit } from 'vscode'
+import { ExtensionContext, languages, TextDocument, TextEdit, Range } from 'vscode'
+
+// @ts-ignore
+import svgToJsx from 'svg-to-jsx'
+
+
+const asyncReplace = async ({
+	string,
+	regex,
+	replacer,
+}: {
+	string: string,
+	regex: RegExp,
+	replacer: (...args: any[]) => Promise<string>,
+}) => {
+	const replacements = await Promise.all(
+		Array.from(
+			string.matchAll(regex),
+			match => replacer(...match),
+		)
+	)
+
+	let i = 0
+	return string.replace(regex, () => replacements[i++])
+}
+
 
 export const activate = (context: ExtensionContext) => {
+	console.log('Extension "svg-to-jsx" is now active!')
+
 	languages.registerDocumentFormattingEditProvider(
 		[
-			'javascriptreact',
-			'typescriptreact',
+			{
+				language: 'javascript',
+			},
+			{
+				language: 'typescript',
+			},
+			{
+				language: 'javascriptreact',
+			},
+			{
+				language: 'typescriptreact',
+			},
 		],
 		{
-			provideDocumentFormattingEdits: (document: TextDocument): TextEdit[] => {
-				const firstLine = document.lineAt(0)
-				return [TextEdit.insert(firstLine.range.start, '42\n')]
+			provideDocumentFormattingEdits: async (document: TextDocument): Promise<TextEdit[]> => {
+				const text = document.getText()
+
+				const svg = await asyncReplace({
+					string: text,
+					regex: /<svg[^>]*>[\s\S]*?<\/svg>/g,
+					replacer: async (match) => await svgToJsx(match)
+				})
+
+				return [
+					TextEdit.replace(new Range(document.positionAt(0), document.positionAt(text.length)), svg)
+				]
 			}
 		}
 	)
